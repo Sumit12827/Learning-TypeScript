ade5a7a2d071a097188e55d372610ea724f3e2b9
Sumit12827 <sumitdowney@gmail.com>
2026-04-09 11:24:57 +0530

    add gitignore and new files

---
+++ arr_enum_tuples.ts
@@ -0,0 +1,28 @@
+// 1. Arrays in TypeScript
+//main concept  => array store multiple values of same types
+
+let numbers: number[] = [12,3,4,5,6];
+let names: string[] = ['sumit' , "sandy" , "suk"]
+
+//there is one more syntax
+let marks: Array<number> = [90 , 9 , 78];
+
+//operations
+numbers.push(56);       // add
+numbers.pop();         // remove
+//numbers.push("hello"); //  Error
+console.log(numbers)
+console.log(marks)
+
+
+// Arrays:
+// - Store multiple values
+// - All elements must be same type
+// Syntax:
+// type[] OR Array<type>
+
+
+
+
+
+
